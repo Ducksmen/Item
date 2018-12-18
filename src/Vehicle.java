e673b031f8e4574c5bc27ef0854cd2122adec88a
BT_1N3_13 <kli8661@bths.edu>
2018-12-18 09:43:42 -0500

    Completed.

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -2,12 +2,14 @@ public class Vehicle extends TaxableItem {
 
     private double dealerCost;
     private double dealerMarkup;
+    private double taxRate;
 
     public Vehicle(double cost, double markup, double rate)
     {
         super(rate);
         dealerCost = cost;
         dealerMarkup = markup;
+        taxRate = rate;
     }
 
     public void changeMarkup(double newMarkup)
@@ -18,5 +20,14 @@ public class Vehicle extends TaxableItem {
     public double getListPrice()
     {
         return dealerCost + dealerMarkup;
+    }
+
+    public String toString()
+    {
+        double num = getListPrice()*(taxRate + 1);
+        double listPrice = Math.round(num*100)/100D;
+        String output = "";
+        output = "Dealer Cost: " +  dealerCost + "\nDealer Markup: " + dealerMarkup + "\nTax Rate: " + taxRate + "\nPurchase Price: " + listPrice;
+        return output;
     }
 }
